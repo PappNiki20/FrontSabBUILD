cddf1eef4fa79f07b8717fb10c5b554b075d54c2
Papp Nikoletta <115682617+PappNiki20@users.noreply.github.com>
2024-02-06 14:15:43 +0100

    alapok

---
+++ FrontendSablonBUILD/alapsablon/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./LayOut";
+import Fooldal from "./pages/Fooldal";
+import Alkoto from "./pages/Alkoto";
+import Csapat from "./pages/Csapat";
+import Kategoria from "./pages/Kategoria";
+import Buszkesegeink from "./pages/Buszkesegeink";
+import Projekt from "./pages/Projekt";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Fooldal />} />
+          <Route path="alkoto" element={<Alkoto />} />
+          <Route path="csapat" element={<Csapat />} />
+          <Route path="kategoria" element={<Kategoria />} />
+          <Route path="buszkesegeink" element={<Buszkesegeink />} />
+          <Route path="projekt" element={<Projekt />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
